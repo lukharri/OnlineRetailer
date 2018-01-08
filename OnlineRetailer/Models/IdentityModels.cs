8a21443d814806da98e3dff76a8205a35da1298b
Luke Harris <luke.e.harris@gmail.com>
2018-01-08 17:05:37 -0600

    Modified AspUsers table - added user properties

--- OnlineRetailer/Models/IdentityModels.cs
+++ OnlineRetailer/Models/IdentityModels.cs
@@ -5,12 +5,19 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using OnlineRetailer.Entities;
 using System.Collections.Generic;
+using System;
 
 namespace OnlineRetailer.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        // Properties added here are reflected as columns in the AspUsers table
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime DateRegistered { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
