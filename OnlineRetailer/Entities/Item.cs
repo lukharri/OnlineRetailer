7e7f3f2f31b1e7e904d3839f56d67a63c6628c49
Luke Harris <luke.e.harris@gmail.com>
2018-01-08 17:21:52 -0600

    Created Item controller and views

--- OnlineRetailer/Entities/Item.cs
+++ OnlineRetailer/Entities/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -16,29 +17,37 @@ namespace OnlineRetailer.Entities
         public string Manufacturer { get; set; }
 
         [Required]
+        [DisplayName("Model Number")]
         public string ModelNumber { get; set; }
 
         [Required]
         public string SKU { get; set; }
 
         [Required]
+        [DisplayName("UPC Code")]
         public int UPCCode { get; set; }
 
         [Required]
+        [DisplayName("Short Description")]
         public string ShortDescription { get; set; }
 
         [Required]
+        [DisplayName("Long Description")]
         public string LongDescription { get; set; }
 
+        [DisplayName("Image URL")]
         public string ImageURL { get; set; }
 
         [Required]
+        [DisplayName("Category Id")]
         public int CategoryId { get; set; }
 
         [Required]
+        [DisplayName("List Price")]
         public float ListPrice { get; set; }
 
         [Required]
+        [DisplayName("Sale Price")]
         public float SalePrice { get; set; }
 
         [Required]
@@ -46,6 +55,7 @@ namespace OnlineRetailer.Entities
             get { return SalePrice / ListPrice; } }
 
         [Required]
+        [DisplayName("In Stock")]
         public bool InStock { get; set; }
     }
 }
