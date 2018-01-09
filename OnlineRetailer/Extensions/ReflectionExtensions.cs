b86c16f4d10567faefdb5ae41201543a5c10972e
Luke Harris <luke.e.harris@gmail.com>
2018-01-09 13:29:43 -0600

    Created method for returning the value of an entity property

---
+++ OnlineRetailer/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineRetailer.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        // return the value of the property of the entity passed into the function
+        public static string GetPropertyValue<T>(this T item, string propertyName)
+        {
+            return item.GetType().GetProperty(propertyName).GetValue(item, null).ToString();
+        }
+    }
+}
